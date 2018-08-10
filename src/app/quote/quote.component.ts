a9310aa48b9096d11fa8045cbf668039a170db94
Edith <you@example.com>
2018-08-10 15:50:08 +0300

    updated quote.component.html and quote.component.ts files

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,11 +8,11 @@ import { Quote } from '../quote'
 export class QuoteComponent implements OnInit {
 
     quotes = [
-      new Quote(1,'get busy living,or get busy dying','Stephen King',0,0,new Date(2018,11,31)),
-      new Quote(2,'get busy living,or get busy dying','Stephen King',0,0,new Date(2018,11,31)),
-      new Quote(3,'get busy living,or get busy dying','Stephen King',0,0,new Date(2018,11,31)),
-      new Quote(4,'get busy living,or get busy dying','Stephen King',0,0,new Date(2018,11,31)),
-      new Quote(5,'get busy living,or get busy dying','Stephen King',0,0,new Date(2018,11,31)),
+      new Quote(1,'"Get busy living,or get busy dying"','Stephen King','upvote', 'downvote',new Date(2018,11,31)),
+      new Quote(2,'"The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself."','Mark Caine','upvote', 'downvote',new Date(2018,11,31)),
+      new Quote(3,'“A successful man is one who can lay a firm foundation with the bricks others have thrown at him.”','David Brinkley','upvote', 'downvote',new Date(2018,11,31)),
+      new Quote(4,'“Those who dare to fail miserably can achieve greatly.”','John F. Kennedy','upvote', 'downvote',new Date(2018,11,31)),
+      new Quote(5,'“It is hard to fail, but it is worse never to have tried to succeed.”','Theodore Roosevelt','upvote', 'downvote',new Date(2018,11,31)),
     ]
 
   constructor() { }
