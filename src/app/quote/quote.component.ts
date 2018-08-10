2ce864bd958c9c3684d6984d137fea71af1bcb04
Edith <you@example.com>
2018-08-10 14:05:36 +0300

    created array

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quote'
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+    quotes = [
+      new Quote(1,'get busy living,or get busy dying','Stephen King',0,0,new Date(2018,11,31)),
+      new Quote(2,'get busy living,or get busy dying','Stephen King',0,0,new Date(2018,11,31)),
+      new Quote(3,'get busy living,or get busy dying','Stephen King',0,0,new Date(2018,11,31)),
+      new Quote(4,'get busy living,or get busy dying','Stephen King',0,0,new Date(2018,11,31)),
+      new Quote(5,'get busy living,or get busy dying','Stephen King',0,0,new Date(2018,11,31)),
+    ]
 
   constructor() { }
 
