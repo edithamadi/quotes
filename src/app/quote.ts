51aa035a8e288aff5154da0bf950c227451df3cd
Edith <you@example.com>
2018-08-11 23:07:04 +0300

    created a toggle effect to allow toggling the quote description on and off

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,3 +1,4 @@
 export class Quote {
+public showDescription:boolean;
   constructor(public identity:number,public msg:string,public author:string,public upvote:number,public downvote:number,public dateadded:Date){}
 }
